7974d8ef208cc43b5f7aa60e21ef9170c70e6a54
GEETHA <GEETHA@DESKTOP-J8GLBS2>
2024-06-25 20:20:05 +0530

    first commit

---
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,49 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	private int serialNum;//data variables
+	private String name;
+	private int age;
+	 
+	void show ()
+	{
+		System.out.println("serial number"+serialNum+"\t name"+name+ "\t age:"+ age);
+	}
+
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+
+	public int getSerialNum() {
+		return serialNum;
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		
+//
+//	}
+
+}
